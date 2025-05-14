dd04f0b81f616bce470746be299306da6726c396
maxim-ge <m.geraskin@untill.com>
2025-05-14 16:23:50 +0200

    ghbrowser

--- rsch/ghbrowser/flutter_bootstrap.js
+++ rsch/ghbrowser/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "896461960"
+    serviceWorkerVersion: "2611659435"
   }
 });
