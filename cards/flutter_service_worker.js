c2b11fe1c16e795f165a41c031914e7a76560683
maxim-ge <m.geraskin@untill.com>
2025-07-09 19:32:48 +0200

    cards

--- cards/flutter_service_worker.js
+++ cards/flutter_service_worker.js
@@ -21,10 +21,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.js.symbols": "9fe690d47b904d72c7d020bd303adf16",
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "9f880368ed480650e7edaba7424f5c3d",
+"flutter_bootstrap.js": "555b4f6fe245955c0ee4dd40b25276c7",
 "index.html": "1cd270d9a96e6138d351bf404007158f",
 "/": "1cd270d9a96e6138d351bf404007158f",
-"main.dart.js": "772f0c31aec4debd36d3091a179c7014",
+"main.dart.js": "1b8c5a6ef85dee640d85c32fa2b4bbff",
 "manifest.json": "cc39383c045c2127040a0e8609afb2e9",
 "version.json": "9287debd5560b751bab1c3a3303d1e56"};
 // The application shell files that are downloaded before a service worker can
