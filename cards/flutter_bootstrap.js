2a4f1e996f727486bcc259cee5512aab7637e552
maxim-ge <m.geraskin@untill.com>
2025-07-23 21:31:14 +0200

    cards

--- cards/flutter_bootstrap.js
+++ cards/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2567904606"
+    serviceWorkerVersion: "4289925271"
   }
 });
