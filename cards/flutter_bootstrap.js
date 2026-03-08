9a10308987a54b998b0072a443c9349ffe3fb7ae
maxim-ge <maxim.ge@gmail.com>
2026-03-08 20:44:01 +0100

    cards

--- cards/flutter_bootstrap.js
+++ cards/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1432549829" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3427209504" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
